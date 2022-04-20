5a1c74facb2153745769dfa752d5c8278866d7fe
André Campos <andre@dimap.ufrn.br>
2022-04-20 07:26:37 -0300

    Aula de 18/04.

--- quiz/src/app/app.component.ts
+++ quiz/src/app/app.component.ts
@@ -1,14 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  template: '<h1>Olá mundo!</h1>',
-  styles: [`
-    h1 {
-      text-align: center;
-    }
-  `]
+  template: `<app-quiz></app-quiz>`
 })
 export class AppComponent {
-  title = 'quiz';
 }
--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { QuestionComponent } from './components/question.component'
+import { QuizComponent } from './components/quiz.component'
+import { ResultsComponent } from './components/results.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizComponent,
+    QuestionComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule
