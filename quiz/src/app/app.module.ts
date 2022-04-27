82a4bd6eb1a59c5270bf7562a749f102a7f2bc7b
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2022-04-27 20:10:04 -0300

    Criação dos componentes para o painel

--- quiz/src/app/app.module.ts
+++ quiz/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule
